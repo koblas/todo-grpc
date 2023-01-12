6ce5155ac9a5eda9e27680b11bd10e305feff572
David Koblas <david@koblas.com>
2023-01-12 03:59:52 -0500

    Add avatar_url

--- ts-client-react/src/rpc/user.ts
+++ ts-client-react/src/rpc/user.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const User = z.object({
   name: z.string(),
   email: z.string(),
-  avatar_url: z.nullable(z.string()),
+  avatar_url: z.optional(z.nullable(z.string())),
 });
 
 export type UserType = z.infer<typeof User>;
