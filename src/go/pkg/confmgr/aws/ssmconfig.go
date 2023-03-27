e62b5c97f0bd1b5152101f1081473ef7d14e9ff9
David Koblas <david@koblas.com>
2023-03-27 15:02:41 -0400

    Fixed SSM loading

--- src/go/pkg/confmgr/aws/ssmconfig.go
+++ src/go/pkg/confmgr/aws/ssmconfig.go
@@ -2,11 +2,14 @@ package aws
 
 import (
 	"context"
+	"regexp"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/koblas/grpc-todo/pkg/confmgr"
+	"github.com/koblas/grpc-todo/pkg/util"
 )
 
 type SsmGetParamertsAPI interface {
@@ -14,8 +17,11 @@ type SsmGetParamertsAPI interface {
 }
 
 type Provider struct {
-	Path   string
-	Client SsmGetParamertsAPI
+	Path      string
+	Client    SsmGetParamertsAPI
+	toSnake   bool
+	cleanRe   *regexp.Regexp
+	seperator string
 }
 
 type Option func(p *Provider)
@@ -42,7 +48,25 @@ func NewLoaderSsm(ctx context.Context, path string, opts ...Option) *Provider {
 		p.Client = ssm.NewFromConfig(cfg)
 	}
 
+	p.seperator = "/"
+	p.cleanRe = regexp.MustCompile(p.seperator + p.seperator + "+")
+
 	return &p
+}
+
+func (p *Provider) getName(spec *confmgr.ConfigSpec) []string {
+	name, ok := spec.Field.Tag.Lookup("ssm")
+	if !ok {
+		name = spec.Field.Name
+		if p.toSnake {
+			name = util.ToSnake(name)
+		}
+	}
+	if spec.Parent == nil {
+		return []string{name}
+	}
+
+	return append(p.getName(spec.Parent), name)
 }
 
 func (p *Provider) Loader(ctx context.Context, conf interface{}, specs []*confmgr.ConfigSpec) ([]*confmgr.ConfigSpec, error) {
@@ -50,15 +74,15 @@ func (p *Provider) Loader(ctx context.Context, conf interface{}, specs []*confmg
 	reverse := map[string]*confmgr.ConfigSpec{}
 
 	for _, spec := range specs {
-		tagValue, ok := spec.Field.Tag.Lookup("ssm")
-		if !ok {
-			tagValue = spec.Field.Name
+		nlist := p.getName(spec)
+		if len(p.Path) != 0 {
+			nlist = append([]string{p.Path}, nlist...)
 		}
+		name := p.cleanRe.ReplaceAllString(strings.Join(nlist, p.seperator), p.seperator)
 
-		qualified := p.Path + tagValue
-		reverse[qualified] = spec
+		reverse[name] = spec
 
-		names = append(names, qualified)
+		names = append(names, name)
 	}
 
 	// If we have no work to do...
