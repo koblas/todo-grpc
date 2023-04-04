86ae9b8ed47a54bbfd091d2089ca24ede00e1870
David Koblas <david@koblas.com>
2023-04-04 05:50:20 -0400

    updated zipfile

--- aws-tf/lib/components/gohandler.ts
+++ aws-tf/lib/components/gohandler.ts
@@ -185,7 +185,7 @@ function createStubAsset(scope: Construct, name: string): TerraformAsset {
   const archive = new zip();
 
   const output = path.join("/tmp/", `${name}-stub.zip`);
-  archive.addFile("boostrap", Buffer.from(new Uint8Array()));
+  archive.addFile("boostrap", Buffer.from("# Just a placeholder", "utf-8"));
   const data = archive.toBuffer();
 
   fs.writeFileSync(output, data);
